cfd689563c53f6dd26f70c2f0b4afbea5ba4772e
mickyphoneaccessories-source <mickyphoneaccessories@gmail.com>
2026-09-23 13:59:52 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,1 @@
-const CACHE='mpa-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='mpa-premium-v3';const A=['./','./index.html','./logo.png','./icon.svg','./manifest.webmanifest','./service-worker.js'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
